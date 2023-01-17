69df4de21de641b46ab96cf3d0167ec01494cb7d
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-17 14:47:33 +0000

    added tests for addEntry

--- spec/secretDiary.spec.js
+++ spec/secretDiary.spec.js
@@ -1,1 +1,52 @@
 // initialize tests
+import lock from "../src/Lock.js";
+import secretDiary from "../src/SecretDiary.js";
+import unlock from "../src/Unlock.js";
+
+
+describe('adding an entry', () => {
+    // test the addEntry when diary is unlocked
+    it('should return the value of the entry', () => {
+        // arrange
+        const myDiary = new secretDiary(new unlock());
+        // act
+        const result = myDiary.addEntry('hello');
+        // assert
+        expect(result).toBe('hello');
+
+    });
+
+    // test the addEntry when diary is locked
+    it('should return Cannot add entries when locked', () => {
+        // arrange
+        const myDiary = new secretDiary(new lock());
+        // act
+        const result = myDiary.addEntry('hello');
+        // assert
+        expect(result).toBe(`Cannot add entries when locked`);
+
+    })
+});
+
+describe('getting entries from diary', () => {
+    // test the getEntries when diary is unlocked
+    it('should show the entries in the diary', () => {
+        // arrange
+
+        // act
+
+        // assert
+
+    });
+
+    // test the getEntries when diary is locked
+    it('should show This diary is locked', () => {
+        // arrange
+
+        // act
+
+        // assert
+
+    });
+    
+});
