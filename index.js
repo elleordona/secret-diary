e72d6f5366fc2c69bff6c537356c3dff401784ae
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-18 16:07:02 +0000

    added index

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import secretDiary from "./src/SecretDiary.js";
-import lock from "./src/Lock.js";
+import Lock from "./src/Lock.js";
 
+// create diaries
+const d1 = new secretDiary(new Lock());
+const d2 = new secretDiary(new Lock());
+
+console.log(d1.addEntry('yo'));
+d2.unlock();
+d2.addEntry('hello');
+
+console.log(d2.getEntries());
